d7a5037a027f7aa70a89c99557b5ba8e13f1d5c3
George Withers <gwithers@buildingengines.com>
2019-11-15 15:57:31 -0500

    final changes:

--- lessons/10-the-feed/exercise/Feed.js
+++ lessons/10-the-feed/exercise/Feed.js
@@ -1,6 +1,8 @@
 import React from "react"
 import FeedPost from "app/FeedPost"
 import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
+import Moment from 'react-moment';
+
 // import FeedFinal from './Feed.final'
 // export default FeedFinal
 export default Feed
@@ -11,6 +13,7 @@ function Feed() {
   const [posts, setPosts] = React.useState([]);
   const [limit, setLimit] = React.useState(FEED_PAGE_SIZE);
   const [newest, setNewest] = React.useState(Date.now());
+  const [newPostsAvailable, setNewPostsAvailable] = React.useState(0);
 
   // React.useEffect(() => {
   //   loadFeedPosts(Date.now(), 3).then (posts => {
@@ -23,29 +26,38 @@ function Feed() {
   React.useEffect(() => {
     loadFeedPosts(newest, limit).then(posts => {
       setPosts(posts);
+      setNewPostsAvailable(0);
     });
   },[limit]);
 
   React.useEffect(() => {
-    subscribeToNewFeedPosts(newest, posts => {
-      posts.map(post => { console.log("NEW POST: " + post )})
+    subscribeToNewFeedPosts(newest, newPosts => {
+      newPosts.map(newPost => { console.log("NEW POST: " + newPost )})
+      setNewPostsAvailable(newPosts.length);
     })
   }, [newest]);
 
   const addNewPosts = () => {
-    setNewest(Date.now())
-    setLimit(limit + FEED_PAGE_SIZE)
+    setNewest(Date.now());
+    setLimit(limit + newPostsAvailable);
+    setNewPostsAvailable(0);
   }
 
-  const addMorePosts = () => {
-    setLimit(limit + FEED_PAGE_SIZE);
-  }
+  // const addMorePosts = () => {
+  //   setLimit(limit + FEED_PAGE_SIZE);
+  // }
 
   return (
     <div className="Feed">
       <div className="Feed_button_wrapper">
-        <button className="Feed_new_posts_button icon_button" onClick={addNewPosts}>
-          View {FEED_PAGE_SIZE} New Posts
+        Showing stuff since <Moment>{newest}</Moment> ...
+
+        <button
+          className="Feed_new_posts_button icon_button "
+          onClick={addNewPosts}
+          disabled={newPostsAvailable === 0}
+        >
+          View {newPostsAvailable} New Posts
         </button>
       </div>
 
@@ -57,7 +69,7 @@ function Feed() {
 
       <div className="Feed_button_wrapper">
         <button
-          onClick={addMorePosts}
+          onClick={() => {setLimit(limit + FEED_PAGE_SIZE)}} // just illustrating inline
           className="Feed_new_posts_button icon_button"
         >
           View More
